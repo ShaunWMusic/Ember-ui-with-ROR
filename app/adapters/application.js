29ca06c2a38c114a178cc606079d3dd1d464f9b0
shaunwmusic <shaunwmusic@gmail.com>
2018-04-26 14:57:34 -0500

    update api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   // localhost
-  host: 'http://localhost:3000'
+  // host: 'http://localhost:3000'
   // Production API
-  // host: 'https://fierce-earth-79141.herokuapp.com'
+  host: 'https://fierce-earth-79141.herokuapp.com'
 });
